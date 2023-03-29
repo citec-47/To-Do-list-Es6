2a1f5d2b4a023e359b5fd958e87ccaef6bedd240
citec-47 <mauricendonyi40@gmail.com>
2023-03-29 05:03:12 -0700

    Iplimentation of start script

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,48 @@
-/* eslint-disable import/no-unresolved */
-import { resolve } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export const mode = 'development';
-export const entry = './src/index.js';
-export const output = {
-  filename: 'main.js',
-  path: resolve(__dirname, 'dist'),
-  clean: true,
-};
-export const module = {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
+module.exports = {
+  entry: './src/index.js',
+  //  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
   ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
 };
-export const plugins = [
-  new HtmlWebpackPlugin({
-    title: 'Output Management',
-    filename: 'index.html',
-    template: './src/index.html',
-  }),
-];
