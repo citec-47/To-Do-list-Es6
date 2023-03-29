cca3e1de3653684d253c8bfb9c1ae1c59bd43654
citec-47 <mauricendonyi40@gmail.com>
2023-03-29 09:29:09 -0700

    populating the HTML list items

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
-/* eslint-disable import/no-unresolved */
-import { resolve } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-export const mode = 'development';
-export const entry = './src/index.js';
-export const output = {
-  filename: 'main.js',
-  path: resolve(__dirname, 'dist'),
-  clean: true,
-};
-export const module = {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
   ],
 };
-export const plugins = [
-  new HtmlWebpackPlugin({
-    title: 'Output Management',
-    filename: 'index.html',
-    template: './src/index.html',
-  }),
-];
